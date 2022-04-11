e54b026ecf6731303a355bd6be0745bbcf044ecc
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-11 13:16:00 -0400

    index action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -7,15 +11,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # This is an INSTANCE of the article model being initialized in the action
-    # Article is referring to the Model, not the Migration
-    # The model is the object that is interacting with the db
-    # Also remember that `params[:attribute]` contains the attributes that we are
-    # interested in
-    # that is the arg passed into the instance of the article
-    
-    # @article = Article.new(params[:article])
-    @article = Article.new(article_params)
+    # @article = Article.new(article_params) # uses the private function
+    @article = Article.new(params.require(:article).permit(:title, :text))
 
     @article.save
     redirect_to @article
