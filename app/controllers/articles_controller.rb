22064d93963dc249e186a24d616d5e28fb05ce57
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-11 11:28:07 -0400

    create show action and show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,29 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    # This is an INSTANCE of the article model being initialized in the action
+    # Article is referring to the Model, not the Migration
+    # The model is the object that is interacting with the db
+    # Also remember that `params[:attribute]` contains the attributes that we are
+    # interested in
+    # that is the arg passed into the instance of the article
+    
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
--- db/migrate/20220411042037_create_articles.rb
+++ db/migrate/20220411042037_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+      # attributes are automatically mapped to db columns
       t.string :title
       t.text :text
 
